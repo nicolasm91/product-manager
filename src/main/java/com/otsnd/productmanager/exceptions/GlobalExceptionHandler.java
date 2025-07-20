80ef46c3e6c181be4470da5b496f4d90109bb162
Nicolas Melichopulos <nicolas.m91@proton.me>
2025-07-20 04:36:01 -0300

    dto validation errors and hints now visible in http response body

--- src/main/java/com/otsnd/productmanager/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/otsnd/productmanager/exceptions/GlobalExceptionHandler.java
@@ -5,20 +5,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        LOGGER.error("error message: {} | errors: {}", exception.getMessage(), exception.getAllErrors());
+
+        Map<String, Object> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .filter(error -> error.getDefaultMessage() != null)
+                .map(objectError -> Map.entry(objectError.getField(), objectError.getDefaultMessage()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        Constants.ERROR_MESSAGE, errors,
+                        Constants.TIMESTAMP, LocalDateTime.now()));
+    }
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException exception) {
-        LOGGER.error(exception.getMessage());
+        LOGGER.error("error message: {}",exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of(
@@ -28,7 +48,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ProductMissingException.class)
     public ResponseEntity<?> handleProductMissingException(ProductMissingException exception) {
-        LOGGER.error(exception.getMessage());
+        LOGGER.error("error message: {}",exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of(
@@ -38,7 +58,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(RepeatedProductInRequestException.class)
     public ResponseEntity<?> handleRepeatedProductInRequestException(RepeatedProductInRequestException exception) {
-        LOGGER.error(exception.getMessage());
+        LOGGER.error("error message: {}",exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of(
@@ -48,7 +68,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ExceededStockException.class)
     public ResponseEntity<?> handleExceededStockException(ExceededStockException exception) {
-        LOGGER.error(exception.getMessage());
+        LOGGER.error("error message: {}",exception.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(Map.of(
